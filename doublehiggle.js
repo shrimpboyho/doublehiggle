58e56efb9d44ec4b4379dcbec8a3a0128b540194
shrimpydubsy <new.joseph10@gmail.com>
2013-10-17 21:48:21 -0400

    Update doublehiggle.js

--- doublehiggle.js
+++ doublehiggle.js
@@ -1,22 +1,25 @@
 (function ($) {
 
     function getHTML(imagesrc) {
-        return "\n \x3Cdiv class=\"modal fade\" id=\"imageViewer\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\"\x3E\n \x3Cdiv class=\"modal-dialog\"\x3E\n \x3Cdiv class=\"modal-content\"\x3E\n \x3Cdiv class=\"modal-header\"\x3E\n \x3Cbutton type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\"\x3E\&times;\x3C\x2Fbutton\x3E\n \x3Ch4 id=\"imageViewerTitle\"\x3EModal title\x3C\x2Fh4\x3E\n \x3C\x2Fdiv\x3E\n \x3Cdiv class=\"modal-body\"\x3E\n \x3Cimg src =" + imagesrc + "\x3E\x3C\x2Fimg\x3E\n \x3C\x2Fdiv\x3E\n \x3Cdiv class=\"modal-footer\"\x3E\n \x3Cbutton type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\"\x3EClose\x3C\x2Fbutton\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E";
+        return "\n \x3Cdiv class=\"modal fade\" id=\"imageViewer\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\"\x3E\n \x3Cdiv class=\"modal-dialog\"\x3E\n \x3Cdiv class=\"modal-content\"\x3E\n \x3Cdiv class=\"modal-header\"\x3E\n \x3Cbutton type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\"\x3E\&times;\x3C\x2Fbutton\x3E\n \x3Ch4 id=\"imageViewerTitle\"\x3EImage\x3C\x2Fh4\x3E\n \x3C\x2Fdiv\x3E\n \x3Cdiv class=\"modal-body\"\x3E\n \x3Cimg style='width:100%;height:100%;'src =" + imagesrc + "\x3E\x3C\x2Fimg\x3E\n \x3C\x2Fdiv\x3E\n \x3Cdiv class=\"modal-footer\"\x3E\n \x3Cbutton type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\"\x3EClose\x3C\x2Fbutton\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E\n \x3C\x2Fdiv\x3E";
     }
 
 
     $.fn.enableEnlarge = function () {
-        this.click(function () {
-            var imagesrc = '"' + $(this).get()[0].src + '"';
+        this.unbind('click').click(function () {
             $('#imageViewer').remove();
+            var imagesrc = '"' + $(this).get()[0].src + '"';
+            $('body').append(getHTML(imagesrc));
             $('#imageViewer').on('show', function () {
+                $(this).css({
+                    'overflow-y':'auto',
+                    'max-height': '80%'
+                });
                 $(this).find('.modalbody').css({
-                    width: 'auto',
-                    height: 'auto',
-                        'max-height': '100%'
+                    width: '100%',
+                    height: '100%'
                 });
             });
-            $('body').append(getHTML(imagesrc));
             $('#imageViewer').modal();
         });
         return this;
